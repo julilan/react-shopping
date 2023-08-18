96b1ba17d7b48474ea135c6822943351fcac442d
julilan <juli.molnar@outlook.com>
2023-08-18 12:08:27 +0300

    first commit

---
+++ src/Test.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button';
+//import { Button } from 'react-bootstrap';
+
+const Test = () => {
+  return (
+    <div>
+      <Button variant='secondary'>Example Button</Button>
+    </div>
+  );
+};
+
+export default Test;
