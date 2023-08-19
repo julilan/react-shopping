41774221a255c18d94f5acedab0d069007fa8c82
julilan <juli.molnar@outlook.com>
2023-08-19 15:15:33 +0300

    add logo to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,19 +1,25 @@
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import { useLocation } from 'react-router-dom';
+import logo from '../img/logo.png';
 
 const NavBar = () => {
   const location = useLocation();
   return (
     <Navbar bg='dark' data-bs-theme='dark' className='justify-content-center'>
-      <Navbar.Brand href='/'>Fit Vista</Navbar.Brand>
+      <Navbar.Brand href='/'>
+        <img
+          src={logo}
+          alt='logo'
+          width='30'
+          height='30'
+          className='d-inline-block align-top'
+        />{' '}
+        Fit Vista
+      </Navbar.Brand>
       <Nav activeKey={location.pathname}>
-        <Nav.Item>
-          <Nav.Link href='/'>Home</Nav.Link>
-        </Nav.Item>
-        <Nav.Item>
-          <Nav.Link href='/products'>Products</Nav.Link>
-        </Nav.Item>
+        <Nav.Link href='/'>Home</Nav.Link>
+        <Nav.Link href='/products'>Products</Nav.Link>
       </Nav>
     </Navbar>
   );
