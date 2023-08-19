ddb2765dff3d1f798219acb7469ca83940d53280
julilan <juli.molnar@outlook.com>
2023-08-19 13:47:30 +0300

    change title and favicon, add more styling to navbar

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,21 @@
 import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import { useLocation } from 'react-router-dom';
 
 const NavBar = () => {
   const location = useLocation();
   return (
-    <Nav
-      variant='underline'
-      className='justify-content-center'
-      activeKey={location.pathname}
-    >
-      <Nav.Item>
-        <Nav.Link href='/'>Home</Nav.Link>
-      </Nav.Item>
-      <Nav.Item>
-        <Nav.Link href='/products'>Products</Nav.Link>
-      </Nav.Item>
-    </Nav>
+    <Navbar bg='dark' data-bs-theme='dark' className='justify-content-center'>
+      <Navbar.Brand href='/'>Fit Vista</Navbar.Brand>
+      <Nav activeKey={location.pathname}>
+        <Nav.Item>
+          <Nav.Link href='/'>Home</Nav.Link>
+        </Nav.Item>
+        <Nav.Item>
+          <Nav.Link href='/products'>Products</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Navbar>
   );
 };
 
